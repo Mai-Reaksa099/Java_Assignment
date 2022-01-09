228b07cb16e7283ab4d89f82700d9aed6e7e5fb9
Mai-Reaksa099 <reaksaengineering007@gmail.com>
2022-01-09 12:23:39 +0700

    add commentd

--- ITECalculator/src/MoneyExchange.java
+++ ITECalculator/src/MoneyExchange.java
@@ -1,36 +1,100 @@
+/**
+ *
+ */
 public class MoneyExchange {
-
+    /**
+     *
+     * @param rail
+     * @return
+     */
     public double Rail2Dolor(int rail){
+
         return (double) rail * 4050;
     }
+
+    /**
+     *
+     * @param rail
+     * @return
+     */
     public double Rail2Euro(int rail){
+
         return rail * 0.00022;
     }
+
+    /**
+     *
+     * @param rail
+     * @return
+     */
     public double Rail2Frank(int rail){
         return rail * 6000;
     }
+
+    /**
+     *
+     * @param rail
+     * @return
+     */
     public double Rail2Pound(int rail){
         return  rail * 6050;
     }
+
+    /**
+     *
+     * @param rail
+     * @return
+     */
     public double Rail2Bart(int rail){
 
         return rail *100;
     }
+
+    /**
+     *
+     * @param dolor
+     * @return
+     */
     public double Dolor2Rail(double dolor){
         return  dolor / 4050;
     }
+
+    /**
+     *
+     * @param euro
+     * @return
+     */
     public double Euro2Rail(double euro){
 
         return euro / 0.00022;
     }
+
+    /**
+     *
+     * @param frank
+     * @return
+     */
     public double Frank2Rail(double frank){
 
         return  frank / 6000;
     }
+
+    /**
+     *
+     * @param pound
+     * @return
+     */
     public double Pound2Rail(double pound){
         return pound / 6050;
     }
+
+    /**
+     *
+     * @param bart
+     * @return
+     */
     public double Bart2Rail(double bart){
         return bart / 100;
     }
+
 }
